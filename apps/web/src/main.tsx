277389d4f45ca8bdc88ce971b35c5c23913cc6d0
sukeshperiyasamy <sukeshsss200@gmail.com>
2026-05-22 01:34:47 +0530

    feat(web): add vercel speed insights integration

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import App from './app/App';
 import './index.css';
@@ -10,5 +11,6 @@ if (!root) throw new Error('Root element not found');
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
+    <SpeedInsights />
   </React.StrictMode>
 );
